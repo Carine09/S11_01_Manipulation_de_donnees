c9af36ece3dbfa5f4367dadff08a1bc63133c2a7
Carine <carinerandrianarijaona@gmail.com>
2025-03-31 14:43:25 +0200

    Exos 1, 2 et 3

---
+++ script1.js
@@ -0,0 +1,21 @@
+/* Avec l’annexe ci-dessous :
+
+const candidates = ['Le Filip','Ruby On The Nail','Leona Winter','Lula Strega','Misty Phoenix',
+  'Perseo','Norma Bell','Edeha Noire','Magnetica','Afrodite Amour'];
+Accède au tableau pour afficher dans la console “Le Filip”
+Accède au tableau pour afficher dans la console “Edeha Noire”
+Parcours le tableau pour afficher dans la console ligne par ligne chaque nom des candidates
+(Bonus) Parcours le tableau candidates pour afficher la liste des candidates dans une page HTML : */
+
+const candidatesExoUn = document.querySelector("#ExerciceUn");
+const candidates = ['Le Filip', 'Ruby On The Nail', 'Leona Winter', 'Lula Strega', 'Misty Phoenix', 'Perseo', 'Norma Bell', 'Edeha Noire', 'Magnetica', 'Afrodite Amour'];
+
+console.log(candidates[0]);
+console.log(candidates[7]);
+console.log(candidates);
+
+
+candidates.forEach((element) => {
+    console.log(element);
+    candidatesExoUn.innerHTML += `<li> ${element} </li>`;
+})
---
+++ script3.js
@@ -0,0 +1,58 @@
+
+/* Ci-dessous deux fichiers annexes à intégrer dans ton projet
+
+data.json
+
+script.js
+
+Pour commencer, affiche dans ta console le type de data pour vérifier qu’il corresponde bien au type object et non string
+
+Affiche dans la console, le premier objet du tableau d’objets
+
+Résultat attendu:
+{
+  "candidate": "Afrodite Amour",
+  "age": 27,
+  "city": "Lyon",
+  "region": "Auvergne-Rhône-Alpes",
+  "ranking": 10
+}
+
+Affiche dans la console, le nombre d’objects contenu dans le tableau
+Résultat attendu: 10
+
+Affiche dans la console, les informations de la candidate arrivée au 4e rang
+Résultat attendu:
+
+{
+  "candidate": "Lula Strega",
+  "age": 27,
+  "city": "Paris",
+  "region": "Île-de-France",
+  "ranking": 4
+}
+
+Affiche dans la console, l’âge moyen des candidates
+Résultat attendu : 27.3 */
+
+const fs = require('fs');
+const filepath = 'data.json';
+
+const content = fs.readFileSync(filepath, {encoding: 'utf8', flag: 'r'});
+const data = JSON.parse(content);
+let sum = 0
+
+// console.log(typeof data);
+// console.log(data[0]);
+console.log(data.length);
+// console.log(data[4]);
+
+data.forEach(element => {
+    // let count = element.age;
+    // console.log(count);
+    // console.log(typeof count);
+    let result = (sum += element.age);
+    console.log(result);
+    let average = result/data.length;
+    console.log(average);
+});
